d5c8095b0fa12f72645ac7f51927ce09409ddd87
Arianne Quiroz <emily.quiroz.lomas@udla.edu.ec>
2026-05-12 11:44:26 -0500

    SEGUNDO EJERCICIO: arreglo de caracteres, implementacio de puntero para impresion

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,11 +3,15 @@
 int main() {
     // Declaración del arreglo
     char vocales[] = {'a','e','i','o','u'};
+    //implementacion de puntero
+    char *ptr;
+    ptr= &vocales[0];
 
     // Imprimir los elementos del arreglo
     for (int i = 0; i < 5; i++) {
 
-        printf(" %c\n", vocales[i] );
+        printf(" %c", vocales[i] );
+        printf(" %c\n", *(ptr+i));
         
     }
 
